3b463d48229b7c1d13b59c569d3988805659b607
codeferrel <anolestal@gmail.com>
2026-05-10 21:29:21 +0700

    add\else&if

--- percabangan.py
+++ percabangan.py
@@ -1,6 +1,5 @@
 # Menggunakan eval atau int agar input terbaca sebagai angka
 total_belanja = int(input("Masukkan total belanja : "))
-
 # Inisialisasi variabel awal
 diskon = 0
 
@@ -18,4 +17,12 @@ bayar = total_belanja - diskon
 
 print(f"Total yang harus dibayar: {bayar}")
 print("Terima kasih telah berbelanja")
-print("Datang lagi yah!")
+print("Datang lagi yah!\n")
+
+
+# cek umur 
+umur=int(input("Masukkan umur anda :"))
+if umur <17 :
+    print("Anda tidak bisa membuat sim")
+else:
+    print(f"anda  bisa membuat sim")

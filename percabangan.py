0fc490d8e63fe0231fd4923127dbeb69a422707c
codeferrel <anolestal@gmail.com>
2026-05-10 18:12:02 +0700

    percabangan.py

---
+++ percabangan.py
@@ -0,0 +1,21 @@
+# Menggunakan eval atau int agar input terbaca sebagai angka
+total_belanja = int(input("Masukkan total belanja : "))
+
+# Inisialisasi variabel awal
+diskon = 0
+
+if total_belanja > 100000:
+    print("Kamu mendapatkan bonus minuman")
+    print("Dan diskon 5%")
+    # Hitung diskon hanya jika belanja > 100.000
+    diskon = total_belanja * 5/100
+else:
+    print("Belanja lagi yuk biar dapat bonus!")
+    diskon = 0
+
+# Hitung total yang harus dibayar
+bayar = total_belanja - diskon
+
+print(f"Total yang harus dibayar: {bayar}")
+print("Terima kasih telah berbelanja")
+print("Datang lagi yah!")
--- variables.py
+++ variables.py
@@ -40,10 +40,13 @@ x=24
 y=20
 my_list=[10.20,30,40,50]
 if(x not in my_list):
-    print("X is not present in given list")
+    print("x is not present in given list")
 else:
-    print("X is present in given list")
+    print("x is present in given list")
+
     if(y in my_list):
-        print("Y is present in given list ")
+   print("y is present in given list ")
+
 else:
-print("Y is Not present in give list ")        
+print(" y is Not present in give list ")        
+

c15de30b688396f04c5fac329da63ffbeb7eedfb
codeferrel <anolestal@gmail.com>
2026-05-12 19:25:07 +0700

    numberMatch

--- conditionalStatment.py
+++ conditionalStatment.py
@@ -8,3 +8,13 @@ if age >= 60:
         print("20% senior discount")
 else:
     print("Not eligible for a senior")
+
+    number = 2
+
+match number:
+    case 1:
+        print("One")
+    case 2 | 3:
+        print("Two or Three")
+    case _:
+        print("Other number")

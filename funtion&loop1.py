eb1a769718c89774c02a01851861bb347a9c39ac
codeferrel <anolestal@gmail.com>
2026-05-15 23:50:20 +0700

    feaat:funtion and loop

---
+++ funtion&loop1.py
@@ -0,0 +1,12 @@
+def hey(bro):
+    print(f"Hallo {bro},Welcome to python ")
+
+
+
+
+jumlah=int(input("Input Perulangan "))
+
+for i in range(jumlah):
+
+    print(f"Perulangan ke :{i+1}")
+    hey("Kenny ")

4084ebd9a7063cbab4e56ad35337259a2fb383f1
codeferrel <anolestal@gmail.com>
2026-05-16 20:33:24 +0700

    feat: parameter and argument

--- variables.py
+++ variables.py
@@ -85,3 +85,11 @@ def hitung_luas_segitiga():
   print("Luas segitiga adalah: ",luas)
    
 hitung_luas_segitiga()
+
+def hitung_luas_segitiga(alas, tinggi):
+  luas = (alas * tinggi) / 2
+  print("Luas segitiga adalah: ",luas)
+    
+hitung_luas_segitiga(5, 7)
+hitung_luas_segitiga(2, 10)
+hitung_luas_segitiga(191, 357)

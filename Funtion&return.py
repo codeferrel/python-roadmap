532a70af3a8f8ede69f435d64f50f0ab8f4244e8
codeferrel <anolestal@gmail.com>
2026-05-16 20:48:50 +0700

    feat:function pangkat

--- Funtion&return.py
+++ Funtion&return.py
@@ -13,3 +13,18 @@ print(tambah())
 print(tambah(1))
 print(tambah(1,2))
 print(tambah(5,6))
+
+
+
+def pangkat(angka, pangkat = 2):
+  hasil = 1
+  for i in range(0,pangkat):
+    hasil = hasil * angka
+  return hasil;
+ 
+print( pangkat(3) )     
+print( pangkat(5) )     
+print( pangkat(10) )     
+print( pangkat(3,3) )   
+print( pangkat(5,4) )    
+print( pangkat(6,6) )    

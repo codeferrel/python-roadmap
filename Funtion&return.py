6b0f1a69fd20f007b61e8364274a7d8209aca510
codeferrel <anolestal@gmail.com>
2026-05-16 20:44:16 +0700

    feat parameter in python

--- Funtion&return.py
+++ Funtion&return.py
@@ -4,3 +4,12 @@ def hitung_luas_segitiga(alas,tinggi):
 
 var1 = hitung_luas_segitiga(5,7)
 print("Luas Segetiga adalah :",var1)
+
+
+def tambah(var2 =5, var3 =2):
+    return var2+var3
+
+print(tambah())
+print(tambah(1))
+print(tambah(1,2))
+print(tambah(5,6))

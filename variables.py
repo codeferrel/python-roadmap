85b30ff74b9013bd635da051f8d7fc969be0dc0f
codeferrel <anolestal@gmail.com>
2026-05-16 20:30:50 +0700

    Feat:Variavel di dalam kolum

--- variables.py
+++ variables.py
@@ -78,4 +78,10 @@ while (count < 3):
     print("Hello Geek")
 
 
-    
+def hitung_luas_segitiga():
+  alas = 5
+  tinggi = 7
+  luas = (alas * tinggi) / 2
+  print("Luas segitiga adalah: ",luas)
+   
+hitung_luas_segitiga()

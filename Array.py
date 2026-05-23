96cec652280243712413122d82fea961fcd58a50
codeferrel <anolestal@gmail.com>
2026-05-23 22:59:04 +0700

    feat:array1

--- Array.py
+++ Array.py
@@ -1,3 +1,20 @@
-a=[1,"Hello",[3.14,"world"]]
-a.append(2) # add the int to the end
-print(a)
+print('#Python progrm to find Greatest value#')
+print('======================================')
+
+n=int(input("Number of list elemet :"))
+print()
+
+x=[]
+for i in range(n):
+ print('Angka ke -',i+1,':',end='')
+ x.append(int(input()))
+
+ print()
+
+max_num=x[0]
+for i in range(1,n):
+ if(x[i] > max_num):
+  max_num=x[i]
+
+
+print('Angka terbesar adalah ',max_num)

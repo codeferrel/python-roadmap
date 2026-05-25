876265cd1c382cb5558d466a64ef8be72c506795
codeferrel <anolestal@gmail.com>
2026-05-25 23:18:28 +0700

    add:array2D

--- Array2D.py
+++ Array2D.py
@@ -1,3 +1,3 @@
-rows, cols = (5, 5)
-arr = [[0]*cols]*rows
-print(arr)
+array_input = [ [10,12,14] ,[0,1,2] ]
+print(array_input[0]) # printing elements of row 0
+print(array_input[1]) # printing elements of row 1

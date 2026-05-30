c8fa29019090fdedd5b7bfaa44cffeefd97b9f00
codeferrel <anolestal@gmail.com>
2026-05-30 21:35:52 +0700

    feat:modify-String

---
+++ modifyString.py
@@ -0,0 +1,15 @@
+#Uper () method return the string in upper case
+#Lower() method return the string in low case
+#strip() method removes any whitespace from the beginning or the end:
+# replace() method replaces a string with another string:
+#split() method splits the string into substrings if it finds instances of the separator:
+a="Hello Yall"
+b= "Hello There"
+c= "What's Up"
+d= "String Modify"
+e= "Upgrade your self now"
+print(a.upper())
+print(b.lower())
+print(c.strip())
+print(d.replace("S","M"))
+print(e.split(",")) # returns ['Hello', ' World!'] 

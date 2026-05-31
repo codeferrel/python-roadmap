e96f818587bbcba7c64b99b7035c6f20e630e928
codeferrel <anolestal@gmail.com>
2026-05-31 23:16:58 +0700

    chore:add list

--- list.py
+++ list.py
@@ -6,3 +6,8 @@ print(mylist)
 list=["apple ","Banana ","Cherry"]
 list[1]="Blackcurrant"
 print(list)
+
+#add list item ->append()
+thislist=["book ","Pen ", "wallet "]
+thislist.append("Fan")
+print(thislist)

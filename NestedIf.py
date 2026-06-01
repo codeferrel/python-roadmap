75e22c73e4c41dda13bce259a89536f418187c2a
codeferrel <anolestal@gmail.com>
2026-06-01 23:39:53 +0700

    feat:pass statment

--- NestedIf.py
+++ NestedIf.py
@@ -6,4 +6,13 @@ if x > 10 :
         print("also above 20")
     else:
         print("not above 20")
-        
+
+    #pass statment    
+value=50
+
+if value < 0:
+    print("negative value")
+elif value ==0 :
+    pass
+else:
+    print("positive value ")

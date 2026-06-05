b4209501315539140a105262091a945a262745ce
codeferrel <anolestal@gmail.com>
2026-06-05 23:19:39 +0700

    chore:revome items

--- AccessItems.py
+++ AccessItems.py
@@ -7,3 +7,11 @@ x = thisdict["model"]
 y= thisdict ["year"]
 print(x)
 print(y)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict) 

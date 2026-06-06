1f08af3d695faccf2a32062496b858cc6bbd2aff
codeferrel <anolestal@gmail.com>
2026-06-06 18:01:37 +0700

    feat:latihan

---
+++ latihan1.py
@@ -0,0 +1,8 @@
+#sitem kasir sederhana
+
+barang=int(input("Masukkan jumlah barang yang ingin anda beli :"))
+
+for i in range(barang):
+    input(f"masukkan barang ke {i+1} :")
+
+print("jumlah barang ",barang)

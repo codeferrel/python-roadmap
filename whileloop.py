2a0636ae5e553163c7914c5acd4cc559b97e9c4f
codeferrel <anolestal@gmail.com>
2026-06-10 13:07:24 +0700

    chore:whileloop

--- whileloop.py
+++ whileloop.py
@@ -1,4 +1,26 @@
-i = 1
-while i < 10:
-  print(i)
-  i += 1
+total_belanja = 0
+nomor_barang = 1
+
+print("=== PROGRAM KASIR SEDERHANA ===")
+print("Ketik '0' jika sudah selesai memasukkan semua harga barang.\n")
+
+while True:
+  
+    harga = int(input(f"Masukkan harga barang ke-{nomor_barang}: Rp "))
+    
+  
+    if harga == 0:
+        print("\nInput selesai. Menghitung total...")
+        break # M
+        
+    
+    if harga < 0:
+        print("Harga tidak boleh minus! Silakan masukkan ulang.")
+        continue 
+    
+    
+    total_belanja += harga
+    nomor_barang += 1 
+print("-" * 30)
+print(f"Total yang harus dibayar: Rp {total_belanja:,}")
+print("Terima kasih telah berbelanja!")

5e6ee76eacb6c0cb6e8a0c3c1b4875b64db91018
codeferrel <anolestal@gmail.com>
2026-06-11 22:54:49 +0700

    feat:format_string

--- whileloop.py
+++ whileloop.py
@@ -3,21 +3,17 @@ nomor_barang = 1
 
 print("=== PROGRAM KASIR SEDERHANA ===")
 print("Ketik '0' jika sudah selesai memasukkan semua harga barang.\n")
-
 while True:
-  
+
     harga = int(input(f"Masukkan harga barang ke-{nomor_barang}: Rp "))
-    
   
     if harga == 0:
         print("\nInput selesai. Menghitung total...")
-        break # M
-        
-    
+        break 
+  
     if harga < 0:
         print("Harga tidak boleh minus! Silakan masukkan ulang.")
         continue 
-    
     
     total_belanja += harga
     nomor_barang += 1 

047804ae1fed449b50bb55f28f951a04f5d96b64
codeferrel <anolestal@gmail.com>
2026-06-11 23:16:50 +0700

    chore:lish_comprehention

--- ALGORITM BASIC/formatstring.py
+++ ALGORITM BASIC/formatstring.py
@@ -1,7 +1,17 @@
+##
 price=59
 txt =f"The price is {price} dollars "
 print(txt)
-
+##
 thislist= ["apple","banana","cherry"]
 del thislist[0]
 print(thislist)
+##
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist) 

eb31f6f1d18ee84153329895f66a66fe9ffc5ef1
codeferrel <anolestal@gmail.com>
2026-06-13 22:54:25 +0700

    chore:update_tuples

--- ALGORITM BASIC/looptooples.py
+++ ALGORITM BASIC/looptooples.py
@@ -1,3 +1,9 @@
+#Tuples are used to store multiple items in a single variable.
+#loop tuple
 thistuple=("apple","banana","cherry")
 for i in thistuple:
   print(i)
+
+#access tuple
+thistuples = ("apple", "banana", "cherry")
+print(thistuples[1])

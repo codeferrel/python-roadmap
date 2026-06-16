a695f93625e0c942a8ae6be783c83d089a2f68f6
codeferrel <anolestal@gmail.com>
2026-06-16 23:52:07 +0700

    chore:joinset update()

--- ALGORITM  PYTHON/Join-set.py
+++ ALGORITM  PYTHON/Join-set.py
@@ -6,3 +6,10 @@ set3 = set1.union(set2)
 set1.update(set2)
 print(set3) 
 print(set1) 
+
+#The update() method inserts the items in set2 into set1:
+set3 = {"a", "b" , "c"}
+set4 = {1, 2, 3}
+
+set3.update(set4)
+print(set3) 

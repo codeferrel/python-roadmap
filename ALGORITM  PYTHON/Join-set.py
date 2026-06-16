0d4fc3db8902df5da963bc1d72210bdc90c1975b
codeferrel <anolestal@gmail.com>
2026-06-16 23:59:03 +0700

    chore:join sets si

--- ALGORITM  PYTHON/Join-set.py
+++ ALGORITM  PYTHON/Join-set.py
@@ -28,3 +28,11 @@ set2 = {"google", "microsoft", "apple"}
 set3 = set1.difference(set2)
 
 print(set3) 
+
+#The symmetric_difference() method will keep only the elements that are NOT present in both sets.
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.symmetric_difference(set2)
+
+print(set3) 

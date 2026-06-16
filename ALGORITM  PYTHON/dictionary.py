eae2ba575522fad0d80acdfa98f8b5b3a207f6d5
codeferrel <anolestal@gmail.com>
2026-06-17 00:05:10 +0700

    chore:access items

--- ALGORITM  PYTHON/dictionary.py
+++ ALGORITM  PYTHON/dictionary.py
@@ -16,3 +16,18 @@ thisdict = {
 }
 mydict = thisdict.copy()
 print(mydict)
+
+#access item ,Add a new item to the original dictionary, and see that the values list gets updated as well:
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.values()
+
+print(x) #before the change
+
+car["color"] = "red"
+
+print(x)

74aeeefa440ab0542970b1c18ffea098c454c093
codeferrel <anolestal@gmail.com>
2026-06-16 23:54:22 +0700

    chore:join set intersection

--- ALGORITM  PYTHON/Join-set.py
+++ ALGORITM  PYTHON/Join-set.py
@@ -13,3 +13,10 @@ set4 = {1, 2, 3}
 
 set3.update(set4)
 print(set3) 
+
+#The intersection() method will return a new set, that only contains the items that are present in both sets.
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.intersection(set2)
+print(set3) 

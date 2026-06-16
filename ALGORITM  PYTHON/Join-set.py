fd3bc8c7e0b41381faf7e518ab7f6af3d913679d
codeferrel <anolestal@gmail.com>
2026-06-16 23:57:19 +0700

    chore:join set difference()

--- ALGORITM  PYTHON/Join-set.py
+++ ALGORITM  PYTHON/Join-set.py
@@ -20,3 +20,11 @@ set2 = {"google", "microsoft", "apple"}
 
 set3 = set1.intersection(set2)
 print(set3) 
+
+#The difference() method will return a new set that will contain only the items from the first set that are not present in the other set
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.difference(set2)
+
+print(set3) 

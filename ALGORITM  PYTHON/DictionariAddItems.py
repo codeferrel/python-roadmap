fe0ccbeb7e7627ed123d180e8243a16f822a9c12
codeferrel <anolestal@gmail.com>
2026-06-20 21:03:17 +0700

    chore:dictionariAddItems

---
+++ ALGORITM  PYTHON/DictionariAddItems.py
@@ -0,0 +1,8 @@
+#Adding an item to the dictionary is done by using a new index key and assigning a value to it:
+thisdict={
+    "brand": "ford",
+    "model": "mustang",
+    "year ":2026
+}
+thisdict ["color"] ="black"
+print(thisdict)

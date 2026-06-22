801e299a6a776e4d13cb5920a4c6b83a432019cb
codeferrel <anolestal@gmail.com>
2026-06-22 21:12:53 +0700

    chore:dictionaries

--- ALGORITM  PYTHON/DictionariAddItems.py
+++ ALGORITM  PYTHON/DictionariAddItems.py
@@ -15,3 +15,12 @@ thisdict = {
 }
 mydict = thisdict.copy()
 print(mydict)
+
+#Make a copy of a dictionary with the dict() function:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict) 

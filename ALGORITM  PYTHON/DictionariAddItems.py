f9c15fe97f45a6c00678281c34cb7606df0a0eb4
codeferrel <anolestal@gmail.com>
2026-06-22 21:10:27 +0700

    chore:dictionaricopy

--- ALGORITM  PYTHON/DictionariAddItems.py
+++ ALGORITM  PYTHON/DictionariAddItems.py
@@ -6,3 +6,12 @@ thisdict={
 }
 thisdict ["color"] ="black"
 print(thisdict)
+
+#Make a copy of a dictionary with the copy() method:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)

eda41d7b033f3690892a9ffa10a5e5712f3bbcd0
codeferrel <anolestal@gmail.com>
2026-06-27 16:55:05 +0700

    chore:nested if

--- ALGORITM  PYTHON/ifelse.py
+++ ALGORITM  PYTHON/ifelse.py
@@ -27,10 +27,29 @@ print("Bigger is", bigger)
 
 #Nested If Statements
 x = 41
-
 if x > 10:
   print("Above ten,")
   if x > 20:
     print("and also above 20!")
   else:
     print("but not above 20.")
+
+    #three level of nesting 
+score = 85
+attendance = 90
+submitted = True
+
+score = 85
+attendance = 90
+submitted = True
+
+if score >= 60:
+  if attendance >= 80:
+    if submitted:
+      print("Pass with good standing")
+    else:
+      print("Pass but missing assignment")
+  else:
+    print("Pass but low attendance")
+else:
+  print("Fail")

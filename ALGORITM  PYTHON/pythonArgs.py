69d368010795eef7bed2da4a68e8ba779f56a766
codeferrel <anolestal@gmail.com>
2026-07-01 23:51:59 +0700

    chore:python*args

--- ALGORITM  PYTHON/pythonArgs.py
+++ ALGORITM  PYTHON/pythonArgs.py
@@ -2,3 +2,12 @@ def my_function(*kids):
   print("The youngest child is " + kids[2])
 
 my_function("Emil", "Tobias", "Linus")
+
+#Accessing individual arguments from *args:
+def my_function(*args):
+  print("Type:", type(args))
+  print("First argument:", args[0])
+  print("Second argument:", args[1])
+  print("All arguments:", args)
+
+my_function("Emil", "Tobias", "Linus") 

9e542a5e90806cebe51e59149a73c2790cafcd99
codeferrel <anolestal@gmail.com>
2026-07-03 23:53:01 +0700

    chore:while:

--- ALGORITM  PYTHON/whileloop.py
+++ ALGORITM  PYTHON/whileloop.py
@@ -1,4 +1,17 @@
-i = 3
-while i < 100:
-  print(i)
-  i = i + 3
+
+
+
+
+
+
+
+# Print numbers until the user enters 0
+number = int(input('Enter a number: '))
+
+# iterate until the user enters 0
+while number != 0:
+    print(f'You entered {number}.')
+    number = int(input('Enter a number: '))
+
+print('The end.')
+

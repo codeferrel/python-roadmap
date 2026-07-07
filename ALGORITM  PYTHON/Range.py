b101e00fe2474325f431f4f78c908a03b5ac718e
codeferrel <anolestal@gmail.com>
2026-07-07 21:11:17 +0700

    feat:range

---
+++ ALGORITM  PYTHON/Range.py
@@ -0,0 +1,11 @@
+#The built-in range() function returns an immutable sequence of numbers, commonly used for looping a specific number of times.
+#This set of numbers has its own data type called range.
+
+x = range(3, 10, 2)
+print(x)
+
+print(list(x))
+#Loop
+for i in range(10):
+  print(i)
+

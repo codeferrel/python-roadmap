1da6f93bc1c0725255ca175f4e5b72a080b8103b
codeferrel <anolestal@gmail.com>
2026-07-07 21:21:34 +0700

    chore:iterable

--- ALGORITM  PYTHON/iterators.py
+++ ALGORITM  PYTHON/iterators.py
@@ -1,10 +1,22 @@
 
 #Iterator vs Iterable
 #Lists, tuples, dictionaries, and sets are all iterable objects. They are iterable containers which you can get an iterator from.
-#All these objects have a iter() method which is used to get an iterator:
+#All these objects have a iter() method which is used to get an iterator
 mytuple = ("apple", "banana", "cherry")
 myit = iter(mytuple)
 
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+
+#Strings are also iterable objects, containing a sequence of characters:
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
 print(next(myit))
 print(next(myit))
 print(next(myit))

621e4200ac8bd6b758deadc7a6a487514804e806
codeferrel <anolestal@gmail.com>
2026-07-09 20:56:19 +0700

    chore:function

--- ALGORITM  PYTHON/Funtion&return.py
+++ ALGORITM  PYTHON/Funtion&return.py
@@ -29,3 +29,14 @@ print( pangkat(3,3) )
 print( pangkat(5,4) )    
 print( pangkat(6,6) )    
 
+#Finding the maximum value
+def my_function(*numbers):
+  if len(numbers) == 0:
+    return None
+  max_num = numbers[0]
+  for num in numbers:
+    if num > max_num:
+      max_num = num
+  return max_num
+
+print(my_function(3, 7, 2, 9, 1)) 

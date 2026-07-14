b90502e399443b384c5660922a09f7768c55871f
codeferrel <anolestal@gmail.com>
2026-07-14 22:16:16 +0700

    chore:pass

---
+++ ALGORITM  PYTHON/pythonPass.py
@@ -0,0 +1,12 @@
+#The pass keyword in a function is used when we define a function but don't want to implement its logic immediately. It allows the function to be syntactically valid, even though it doesn't perform any actions yet
+
+class emptyClass:
+    pass # no method or atribut yet
+
+class person:
+    def_init_(self,name,age):
+      self.name = name 
+      self.age = age
+def greet(self):
+   pass
+p=person("CodeF",21)

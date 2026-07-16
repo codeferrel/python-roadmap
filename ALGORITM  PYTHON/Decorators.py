58fbe5347a30faf7127434e32d041830a822f76f
codeferrel <anolestal@gmail.com>
2026-07-16 20:26:31 +0700

    fear:Decorators.Py

---
+++ ALGORITM  PYTHON/Decorators.py
@@ -0,0 +1,40 @@
+#By placing @changecase directly above the function definition, the function myfunction is being "decorated" with the changecase function.
+# The function changecase is the decorator.
+# The function myfunction is the function that gets decorated.
+# Using the @changecase decorator on two functions:
+
+def changecase(func):
+    def myinner():
+        return func().upper()
+    return myinner
+
+@changecase
+def myfunction():
+    return "Hello Mbappe"
+
+@changecase
+def otherfunction():
+    return "I'm Kenjo Fernandes"
+
+
+print(myfunction())
+print(otherfunction())
+
+
+###############################
+
+# def changecase(func):
+#     def myinner():
+#         return func().upper()
+#     return myinner
+
+# @changecase
+# def func2():
+#     return "HELLO 1"
+
+# @changecase
+# def func3():
+#     return "HELLO 2"
+
+# print(func2())
+# print(func3())

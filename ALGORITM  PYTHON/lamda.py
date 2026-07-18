22e186788dce26dcf77c25e1d46fd47e51ed5863
codeferrel <anolestal@gmail.com>
2026-07-18 22:34:04 +0700

    chore:recurtion.py

--- ALGORITM  PYTHON/lamda.py
+++ ALGORITM  PYTHON/lamda.py
@@ -1,3 +1,14 @@
 #A lambda function can take any number of arguments, but can only have one expression.
 x = lambda a, b : a * b
 print(x(5, 6)) 
+
+
+# A simple recursive function that counts down from 5:
+def countdown(n):
+  if n <= 0:
+    print("Done!")
+  else:
+    print(n)
+    countdown(n - 1)
+
+countdown(5)

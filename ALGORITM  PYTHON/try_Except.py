b9e75552c1dff56aebf06c351544b9074da82335
codeferrel <anolestal@gmail.com>
2026-07-22 23:02:33 +0700

    feat:tru...Except

---
+++ ALGORITM  PYTHON/try_Except.py
@@ -0,0 +1,24 @@
+# The try block lets you test a block of code for errors.
+# The except block lets you handle the error.
+# The else block lets you execute code when there is no error.
+# The finally block lets you execute code, regardless of the result of the try- and except blocks.
+##########################################
+
+
+# The try block will generate an exception, because x is not defined:
+try:
+    print(x)
+except:
+    print("An exception occurred")
+##########################################
+
+
+
+    #Print one message if the try block raises a NameError and another for other errors:
+
+try:
+  print(x)
+except NameError:
+  print("Variable x is not defined")
+except:
+  print("Something else went wrong")

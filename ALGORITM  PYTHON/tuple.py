9d24f43b1db561f6da322042fee9e47d5e6aa018
codeferrel <anolestal@gmail.com>
2026-07-24 23:01:44 +0700

    chore:tuple

--- ALGORITM  PYTHON/tuple.py
+++ ALGORITM  PYTHON/tuple.py
@@ -15,3 +15,22 @@ print(tuple(li))
 # Using Built-in Function
 tup = tuple('Geeks')
 print(tup)
+
+
+###########################################
+# Dictionary dengan tuple sebagai key
+lokasi_points = {
+    (1, 2): "Point A",
+    (3, 4): "Point B",
+    (5, 6): "Point C"
+}
+
+# Mengakses nilai
+print(lokasi_points[(1, 2)])  # Output: "Point A"
+
+# Contoh kompleks: mapping koordinat ke informasi
+peta_kota = {
+    (3.595, 98.678): "Kantor Pusat",
+    (3.597, 98.680): "Gudang Utama",
+    (3.600, 98.675): "Cabang Timur"
+}

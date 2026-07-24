dfbc825be6d8ecf47bc9bc7f54a73b39f18fb8ea
codeferrel <anolestal@gmail.com>
2026-07-24 22:12:46 +0700

    chore:tuple.py

---
+++ ALGORITM  PYTHON/tuple.py
@@ -0,0 +1,17 @@
+
+# A tuple is created by placing all the items inside parentheses (), separated by commas. A tuple can have any number of items.
+
+tup = ()
+print(tup)
+
+# Using String
+tup = ('Geeks', 'For')
+print(tup)
+
+# Using List
+li = [1, 2, 4, 5, 6]
+print(tuple(li))
+
+# Using Built-in Function
+tup = tuple('Geeks')
+print(tup)

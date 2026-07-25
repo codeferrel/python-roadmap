af247424f21e4ecf6b5fb17f82a8a5566069b22b
codeferrel <anolestal@gmail.com>
2026-07-25 23:46:25 +0700

    feat:slicing

---
+++ ALGORITM  PYTHON/Slicing.py
@@ -0,0 +1,9 @@
+tup2 = (1, 2, 3, 4, 5, 6, 7)
+print(tup2[1:5])  # Output: (2, 3, 4, 5)
+
+# Contoh slicing lanjutan
+my_tuple = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+slice1 = my_tuple[2:6]    # Output: (2, 3, 4, 5)
+slice2 = my_tuple[:4]     # Output: (0, 1, 2, 3)
+slice3 = my_tuple[5:]     # Output: (5, 6, 7, 8, 9)
+slice4 = my_tuple[1:8:2]  # Output: (1, 3, 5, 7)

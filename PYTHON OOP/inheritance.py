d4924f9e3ceebab15e8b94d860bd40c28a301621
codeferrel <anolestal@gmail.com>
2026-08-03 00:47:49 +0700

    chore:inheritance

--- PYTHON OOP/inheritance.py
+++ PYTHON OOP/inheritance.py
@@ -15,3 +15,19 @@ class Person:
 
 x = Person("John", "Doe")
 x.printname() 
+
+# To keep the inheritance of the parent's __init__() function, add a call to the parent's __init__() function:
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
+
+x = Student("Kenjo", "Lesta")
+x.printname()

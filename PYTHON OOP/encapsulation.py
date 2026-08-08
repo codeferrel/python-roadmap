e619eacb48cb061d9184c593bd8b18bb57ea83e8
codeferrel <anolestal@gmail.com>
2026-08-08 22:12:19 +0700

    add:encaptulation

--- PYTHON OOP/encapsulation.py
+++ PYTHON OOP/encapsulation.py
@@ -19,3 +19,16 @@ class Person:
         print(p1.__age)
 
         pass
+
+    ##################################
+# Use a getter method to access a private property:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age
+
+  def get_age(self):
+    return self.__age
+
+p1 = Person("Tobias", 25)
+print(p1.get_age()) 

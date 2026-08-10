2ca6f646baf770c24a970505106e6a9fbdeb56d4
codeferrel <anolestal@gmail.com>
2026-08-10 23:58:25 +0700

    chore:encaptulation

--- PYTHON OOP/encapsulation.py
+++ PYTHON OOP/encapsulation.py
@@ -20,7 +20,7 @@ class Person:
 
         pass
 
-    ##################################
+##################################
 # Use a getter method to access a private property:
 class Person:
   def __init__(self, name, age):
@@ -31,4 +31,27 @@ class Person:
     return self.__age
 
 p1 = Person("Tobias", 25)
+print(p1.get_age()) 
+
+
+######################################
+# Use a setter method to change a private property:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age
+
+  def get_age(self):
+    return self.__age
+
+  def set_age(self, age):
+    if age > 0:
+      self.__age = age
+    else:
+      print("Age must be positive")
+
+p1 = Person("code", 21)
+print(p1.get_age())
+
+p1.set_age(22)
 print(p1.get_age()) 

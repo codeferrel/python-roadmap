7954f0fe7711545f0340e2562f92416af2aa5624
codeferrel <anolestal@gmail.com>
2026-08-24 19:20:51 +0700

    chore:numpyindex

--- NumpyTutorial/numpyarray.py
+++ NumpyTutorial/numpyarray.py
@@ -11,3 +11,13 @@ print(arr)
 print(type(arr))
 
 # type(): This built-in Python function tells us the type of the object passed to it. Like in above code it shows that arr is numpy.ndarray type.
+
+
+
+# Example
+# Get third and fourth elements from the following array and add them.
+import numpy as np
+
+arr = np.array([1, 2, 3, 4])
+
+print(arr[2] + arr[3])

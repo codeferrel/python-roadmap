f382428f30ad689228b161d21a6ab3207865bafc
codeferrel <anolestal@gmail.com>
2026-08-25 20:21:51 +0700

    chore:arrayslicing

--- NumpyTutorial/numpyArrayslicing.py
+++ NumpyTutorial/numpyArrayslicing.py
@@ -25,3 +25,11 @@ import numpy as np
 arr = np.array([1, 2, 3, 4, 5, 6, 7])
 
 print(arr[4:]) 
+
+
+# Slice elements from the beginning to index 4 (not included):
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[:4]) 

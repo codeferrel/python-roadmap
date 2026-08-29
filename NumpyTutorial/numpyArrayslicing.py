e26f9fa20a45a5dc36241551d2ff0ec30772db48
codeferrel <anolestal@gmail.com>
2026-08-29 20:51:18 +0700

    chore:numpyarr2d

--- NumpyTutorial/numpyArrayslicing.py
+++ NumpyTutorial/numpyArrayslicing.py
@@ -33,3 +33,11 @@ import numpy as np
 arr = np.array([1, 2, 3, 4, 5, 6, 7])
 
 print(arr[:4]) 
+
+
+# Access the element on the first row, second column:
+import numpy as np
+
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+print('2nd element on 1st row: ', arr[0, 1])

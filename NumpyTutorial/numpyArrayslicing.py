a12e5cb948e83a7d2d7dfbb4200b1895029f7b8a
codeferrel <anolestal@gmail.com>
2026-08-30 23:59:25 +0700

    chore:arrayslicing

--- NumpyTutorial/numpyArrayslicing.py
+++ NumpyTutorial/numpyArrayslicing.py
@@ -41,3 +41,11 @@ import numpy as np
 arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
 
 print('2nd element on 1st row: ', arr[0, 1])
+
+
+# Slice elements from index 1 to index 5 from the following array:
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[1:5]) 

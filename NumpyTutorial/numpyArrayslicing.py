570111f52ef5ead0d77d369c8610146c3c515f2e
codeferrel <anolestal@gmail.com>
2026-08-31 23:08:27 +0700

    chore:copyvsview

--- NumpyTutorial/numpyArrayslicing.py
+++ NumpyTutorial/numpyArrayslicing.py
@@ -48,4 +48,13 @@ import numpy as np
 
 arr = np.array([1, 2, 3, 4, 5, 6, 7])
 
-print(arr[1:5]) 
+
+# Make a copy, change the original array, and display both arrays:
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.copy()
+arr[0] = 42
+
+print(arr)
+print(x) 

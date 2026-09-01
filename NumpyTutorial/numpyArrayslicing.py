a6d3b05f5346177345d86f6ed98f6511e3ba7584
codeferrel <anolestal@gmail.com>
2026-09-01 23:15:18 +0700

    add:shapeofarray

--- NumpyTutorial/numpyArrayslicing.py
+++ NumpyTutorial/numpyArrayslicing.py
@@ -58,3 +58,16 @@ arr[0] = 42
 
 print(arr)
 print(x) 
+
+
+
+
+
+# Get the Shape of an Array
+# NumPy arrays have an attribute called shape that returns a tuple with each index having the number of corresponding elements
+
+import numpy as np
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+
+print(arr.shape)

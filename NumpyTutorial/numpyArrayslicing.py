f9dfb28012f685de3e186d1ed2bb28fe48611773
codeferrel <anolestal@gmail.com>
2026-09-07 22:41:28 +0700

    feat:iteratingarray

--- NumpyTutorial/numpyArrayslicing.py
+++ NumpyTutorial/numpyArrayslicing.py
@@ -79,3 +79,19 @@ arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 newarr = arr.reshape(4, 3)
 
 print(newarr) 
+
+
+
+# Iterating Arrays
+
+# Iterating means going through elements one by one.
+
+# As we deal with multi-dimensional arrays in numpy, we can do this using basic for loop of python.
+
+# If we iterate on a 1-D array it will go through each element one by one.
+import numpy as np
+
+arr = np.array([1, 2, 3])
+
+for x in arr:
+  print(x)

40d663cbbac42e0edbf2781e6a01f47195b96ae8
codeferrel <anolestal@gmail.com>
2026-09-13 09:49:28 +0700

    feat:numpyjoin

--- NumpyTutorial/numpyArrayslicing.py
+++ NumpyTutorial/numpyArrayslicing.py
@@ -83,11 +83,8 @@ print(newarr)
 
 
 # Iterating Arrays
-
 # Iterating means going through elements one by one.
-
 # As we deal with multi-dimensional arrays in numpy, we can do this using basic for loop of python.
-
 # If we iterate on a 1-D array it will go through each element one by one.
 import numpy as np
 
@@ -95,3 +92,5 @@ arr = np.array([1, 2, 3])
 
 for x in arr:
   print(x)
+
+

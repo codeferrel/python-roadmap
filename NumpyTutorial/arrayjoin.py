49e95521d0c4a82d406cdeb36b00b96de825a5d9
codeferrel <anolestal@gmail.com>
2026-09-13 10:00:04 +0700

    chore:join_hstack

--- NumpyTutorial/arrayjoin.py
+++ NumpyTutorial/arrayjoin.py
@@ -1,7 +1,7 @@
 # Joining means putting contents of two or more arrays in a single array.
 # In SQL we join tables based on a key, whereas in NumPy we join arrays by axes.
 # We pass a sequence of arrays that we want to join to the concatenate() function, along with the axis. If axis is not explicitly passed, it is taken as 0.
-import numpy as np
+# import numpy as np
 
 arr1=np.array([1,2,3])
 
@@ -22,3 +22,13 @@ arr1 = np.array([1, 2, 3])
 arr2 = np.array([4, 5, 6])
 
 arr=np.stack((arr1,arr2),axis=1)
+
+# NumPy provides a helper function: hstack() to stack along rows.
+
+import numpy as np
+
+arr1=np.array([1,2,3])
+arr2=np.array([3,4,5])
+arr=np.hstack((arr1,arr2))
+
+print(arr)

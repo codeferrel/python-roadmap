42ad14781b614f9dc9e53239181df7129d2ad71c
codeferrel <anolestal@gmail.com>
2026-09-16 20:41:22 +0700

    chore:search

--- NumpyTutorial/searcharray.py
+++ NumpyTutorial/searcharray.py
@@ -8,3 +8,15 @@ arr = np.array([1, 2, 3, 4, 5, 4, 4])
 x = np.where(arr == 4)
 
 print(x) 
+
+# The example above will return a tuple: (array([3, 5, 6],)
+# Which means that the value 4 is present at index 3, 5, and 6.
+
+# Find the indexes where the values are odd:
+import numpy as np
+
+arr = np.array([10, 14, 93, 41, 8, 7])
+
+x = np.where(arr%2 == 1)
+
+print(x) 

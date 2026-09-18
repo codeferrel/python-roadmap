c2add3ba3ac4fa652d91ebd5ffef3e135fb481ed
codeferrel <anolestal@gmail.com>
2026-09-18 23:04:42 +0700

    chore:sort

--- NumpyTutorial/numpysort.py
+++ NumpyTutorial/numpysort.py
@@ -8,3 +8,12 @@ import numpy as np
 arr = np.array([3, 2, 0, 1])
 
 print(np.sort(arr))
+
+
+
+# Sort the array alphabetically:
+import numpy as np
+
+arr = np.array(['banana', 'cherry', 'apple'])
+
+print(np.sort(arr)) 

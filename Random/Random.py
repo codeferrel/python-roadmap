299e327af835c0b8fde92651104ed9366ab2a781
codeferrel <anolestal@gmail.com>
2026-09-24 22:29:13 +0700

    feat:randompy

---
+++ Random/Random.py
@@ -0,0 +1,7 @@
+# Random number does NOT mean a different number every time. Random means something that can not be predicted logically.
+# Generate a random integer from 0 to 100:
+from numpy import random
+
+x = random.randint(100)
+
+print(x) 

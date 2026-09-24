4e4139d04953a9504d51afb91bdf29da48e086ee
codeferrel <anolestal@gmail.com>
2026-09-24 22:31:05 +0700

    chore:random

--- Random/Random.py
+++ Random/Random.py
@@ -5,3 +5,10 @@ from numpy import random
 x = random.randint(100)
 
 print(x) 
+
+# Generate a 1-D array containing 5 random integers from 0 to 100:
+from numpy import random
+
+x=random.randint(100, size=(5))
+
+print(x) 
